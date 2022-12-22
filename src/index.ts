2ea034b5750094d2d608f6d366f2775b8ad459d9
junsungkim <jskim2@wemb.co.kr>
2022-12-22 15:54:56 +0900

    test es5

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,17 @@ import _ from 'lodash'
 
 
 console.log('123123123')
+
+class Test {
+    name
+    constructor() {
+        this.name = 'john'
+    }
+
+    logName() {
+        console.log(this.name)
+    }
+}
+
+const test = new Test()
+test.logName()
--- webpack.dev.js
+++ webpack.dev.js
@@ -6,7 +6,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
- module: {
+  module: {
     rules: [
         {
           test: /\.css$/i,
