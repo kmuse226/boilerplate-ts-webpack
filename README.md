4c972a2f307f91ba240b50f29822098ed5dd8c93
jskim <kmuse226@gmail.com>
2022-12-29 19:52:40 +0900

    formatter & ruler

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
 # ESLINT 설정 순서
+
 1. yarn add -D eslint
 2. npx eslint --init
 3. npx eslint ./src
 
 # Prettier 설정 순서
+
 1. yarn add -D prettier
 2. .prettierrc.js 생성
 3. npx prettier --check .src OR npx prettier --write ./src
+
+# Editor Ruler
+
+1. setting.json
+2. editor.rulers: [80]
+3. prettier printWidth: 80,
+
+# Formatter (default)
+
+1. prettier extension 설치
+2. setting > format on Save true
--- webpack.common.js
+++ webpack.common.js
@@ -1,54 +1,54 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
-const path = require('path')
-const dotenv = require('dotenv')
+const path = require('path');
+const dotenv = require('dotenv');
 
-
-dotenv.config({path:path.join(__dirname,'.env')});
-console.log(process.env.ENV_VAR1)
-console.log(process.env.ENV_VAR2)
-console.log(process.env.ENV_VAR3)
+dotenv.config({ path: path.join(__dirname, '.env') });
+console.log(process.env.ENV_VAR1);
+console.log(process.env.ENV_VAR2);
+console.log(process.env.ENV_VAR3);
 
 // dotenv.config({path:path.join(__dirname,'.env.production')});
 // console.log(process.env.PRODUCT_VAR1)
 // console.log(process.env.PRODUCT_VAR2)
 // console.log(process.env.PRODUCT_VAR3)
 
-console.log(process.env.NODE_ENV,'webpack node key')
-console.log(process.env.NODE_ENV == 'production' ? 'progress build' : 'progress dev')
-
+console.log(process.env.NODE_ENV, 'webpack node key');
+console.log(
+  process.env.NODE_ENV == 'production' ? 'progress build' : 'progress dev',
+);
 
 module.exports = {
-    entry: ["core-js/stable",'./src/index.ts'],
-    output: {
-        path: __dirname + '/dist',
-        filename: 'js/main.js',
-        assetModuleFilename: 'images/[hash][ext][query]'
+  entry: ['core-js/stable', './src/index.ts'],
+  output: {
+    path: __dirname + '/dist',
+    filename: 'js/main.js',
+    assetModuleFilename: 'images/[hash][ext][query]',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|webp)$/i,
+        type: 'asset/resource',
       },
-    resolve: {
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(png|jpg|webp)$/i,
-          type: 'asset/resource'
+      {
+        test: /\.m?ts$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
         },
-        {
-          test: /\.m?ts$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env']
-            }
-          }
-        },
-        {
-          test: /\.(tsx|ts)?$/i,
-          exclude: /node_modules/,
-          use: ['ts-loader']
-        }
-      ],
-    }
-  };
+      },
+      {
+        test: /\.(tsx|ts)?$/i,
+        exclude: /node_modules/,
+        use: ['ts-loader'],
+      },
+    ],
+  },
+};
